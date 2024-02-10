cc70aa91d5e2d226fd53a9f8007b4b754d7c3a26
Teodora Kovacevic <kovacevicteodora6@gmial.com>
2024-02-10 14:25:29 +0100

    NavBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,18 @@ import{Routes,Route,Navigate} from "react-router-dom"
 import Chat from "./pages/Chat";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Container} from "react-bootstrap"
+import NavBar from "./components/NavBar"
 
 
 function App() {
   
   return(
-     <>
-  <Routes>
+    <>
+    <NavBar/>
+    <Container >
+    <Routes>
     <Route path="/" element={<Chat/>}/>
     <Route path="/register" element={<Register/>}/>
     <Route path="/login" element={<Login/>}/>
@@ -16,8 +21,12 @@ function App() {
 
     
     </Routes> 
+    </Container>
+    
     </>
+     
   );
 }
+
 
 export default App
