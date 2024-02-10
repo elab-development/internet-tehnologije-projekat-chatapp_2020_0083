c1670d58a751951fe6839d670a3dfbb940a2bdbb
Teodora Kovacevic <kovacevicteodora6@gmial.com>
2024-02-10 13:19:43 +0100

    Find user

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -56,13 +56,44 @@ const loginUser=async(req,res)=>{
 
 
     }catch(error){
-
+        console.log(error);
+        res.status(500).json(error);
 
     }
-}
+};
+
+const findUser=async(req, res)=> {
+
+    const userId = req.params.userId;
+    try {
+        const user = await userModel.findById(userId);
+
+        res.status(200).json(user);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+
+};
+const getUsers=async(req, res)=> {
+
+    
+    try {
+        const users = await userModel.find();
+
+        res.status(200).json(users);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+
+};
+
 
 
 
 module.exports = {
-    registerUser,loginUser
-}
+    registerUser,loginUser,findUser, getUsers
+};
