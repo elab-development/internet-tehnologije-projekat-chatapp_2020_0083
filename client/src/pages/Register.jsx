666e64bb9336ccfa9d6152d9a7f306fe198e1498
KristinaKuzmic <kristinakuzmic52@gmail.com>
2024-02-11 16:37:20 +0000

    Register user na frontu

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap"
+import { AuthContext } from "../context/AuthContext";
 
 const Register = () => {
+    const {registerInfo,updateRegisterInfo}=useContext(AuthContext)
     return (<>
         <Form>
             <Row style={{
@@ -13,9 +16,10 @@ const Register = () => {
                     <h2>
                         Register
                     </h2>
-                    <Form.Control type="text" placeholder="Name"/>
-                    <Form.Control type="email" placeholder="Email"/>
-                    <Form.Control type="password" placeholder="Password"/>
+                   
+                    <Form.Control type="text" placeholder="Name" onChange={(e)=>updateRegisterInfo({...registerInfo,name:e.target.value})}/>
+                    <Form.Control type="email" placeholder="Email"  onChange={(e)=>updateRegisterInfo({...registerInfo,email:e.target.value})}/>
+                    <Form.Control type="password" placeholder="Password"  onChange={(e)=>updateRegisterInfo({...registerInfo,password:e.target.value})}/>
                     <Button variant="primary" type="submit">
                         Register
                     </Button>
