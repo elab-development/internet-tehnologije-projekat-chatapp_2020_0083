f332d7a36d079176bfb2e075d2a45fb6cd8c62e9
KristinaKuzmic <kristinakuzmic52@gmail.com>
2024-02-12 20:54:23 +0000

    Registracija user-a na front delu

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,9 +1,12 @@
 import { createContext, useCallback, useState } from "react";
+import { baseUrl, postRequest } from "../utils/services";
 
 export const AuthContext = createContext();
 
 export const AUTHcontextProvider = ({children})=>{
     const[user, setUser]=useState(null);
+    const [registerError,setRegisterError]=useState(null);
+    const [isRegisterLoading,setIsRegisterLoading]=useState(false);
     const [registerInfo, setRegisterInfo]=useState({
         name:"",
         email:"",
@@ -13,10 +16,29 @@ export const AUTHcontextProvider = ({children})=>{
 
     const updateRegisterInfo = useCallback((info)=>{
         setRegisterInfo(info);
-    },[])
+    },[]);
+
+    const registerUser = useCallback(async(e)=>{
+        e.preventDefault();
+
+        setIsRegisterLoading(true);
+        setRegisterError(null);
+
+       const response= await postRequest(`${baseUrl}/users/register`,JSON.stringify(registerInfo));
+       setIsRegisterLoading(false) ;
+       
+       if(response.error){
+            return setRegisterError(response);
+        }
+
+        localStorage.setItem("User",JSON.stringify(response))
+
+        setUser(response)
+    
+    },[registerInfo])
 
     return <AuthContext.Provider value={{
-        user,registerInfo,updateRegisterInfo,
+        user,registerInfo,updateRegisterInfo, registerError, registerUser, isRegisterLoading
     }}>
     {children}
     </AuthContext.Provider>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,9 +3,9 @@ import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap"
 import { AuthContext } from "../context/AuthContext";
 
 const Register = () => {
-    const {registerInfo,updateRegisterInfo}=useContext(AuthContext)
+    const {registerInfo,updateRegisterInfo,registerUser, registerError,isRegisterLoading }=useContext(AuthContext)
     return (<>
-        <Form>
+        <Form onSubmit={registerUser}>
             <Row style={{
                 height:"100vh",
                 justifyContent:"center",
@@ -21,16 +21,20 @@ const Register = () => {
                     <Form.Control type="email" placeholder="Email"  onChange={(e)=>updateRegisterInfo({...registerInfo,email:e.target.value})}/>
                     <Form.Control type="password" placeholder="Password"  onChange={(e)=>updateRegisterInfo({...registerInfo,password:e.target.value})}/>
                     <Button variant="primary" type="submit">
-                        Register
+                        {isRegisterLoading ? "Creating your account":"Register"}
                     </Button>
+{
+    registerError?.error && <Alert variant="danger">
+    <p>
+      {
+        registerError?.message
+      }
+    </p>
 
-                    <Alert variant="danger">
-                        <p>
-                            An error occured
-                        </p>
 
-
-                    </Alert>
+</Alert>
+}
+                    
 
                 </Stack>
                 </Col>
