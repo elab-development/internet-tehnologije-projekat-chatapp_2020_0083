85708387ff4652e9a63c8d1bcf22e73b1e938c85
KristinaKuzmic <kristinakuzmic52@gmail.com>
2024-02-20 13:29:49 +0000

    povezan Chat sa serverske stane sa klijenskom stranom

--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -25,3 +25,25 @@ if(!response.ok){
 
 return data;
 }
+
+export const getRequest =async(url)=>{
+   const response= await fetch(url)
+    const data = await response.json()
+
+    if(!response.ok){
+        let message = "An error occured"
+
+        if(data?.message){
+            message=data.message
+        }
+
+        return {
+            error:true, message
+        }
+    }
+    return data;
+
+
+}
+
+
